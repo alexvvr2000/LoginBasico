619971e2381ab6c658c4d8314fe2801dbe201cf1
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-10-04 00:25:41 -0600

    Agregado metodo on submit para objeto usuario

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
+export interface Usuario {
+  nombre: string;
+  clave: string;
+  correo: string;
+}
+
 @Component({
   selector: 'app-login-form',
-  imports: [IonicModule, RouterModule],
+  imports: [IonicModule, RouterModule, ReactiveFormsModule],
   standalone: true,
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
+  usuarioNuevo: FormGroup = new FormGroup({
+    nombre: new FormControl(''),
+    clave: new FormControl(''),
+    correo: new FormControl(''),
+  });
+
   constructor() {}
+
+  onSubmit(): void {
+    const nombreUsaurio = this.usuarioNuevo.get('nombre')?.value;
+  }
 }
