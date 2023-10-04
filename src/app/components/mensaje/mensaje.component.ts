578533359788312417a67f6e3de8672b36c5a0ca
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-10-04 14:35:27 -0600

    Ahora componente mensaje es de ionic

--- src/app/components/mensaje/mensaje.component.ts
+++ src/app/components/mensaje/mensaje.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
+  imports: [RouterModule, IonicModule],
   selector: 'app-mensaje',
   templateUrl: './mensaje.component.html',
   styleUrls: ['./mensaje.component.scss'],
+  standalone: true,
 })
-export class MensajeComponent  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class MensajeComponent {
+  constructor() {}
 }
