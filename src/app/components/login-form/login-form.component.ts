781a2715be7bd1f0f2beff968a09acc3ca13dfae
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-10-04 15:35:03 -0600

    Nuevos validadores agregados

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
@@ -17,17 +22,23 @@ export interface Usuario {
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
-  usuarioNuevo: FormGroup = new FormGroup({
-    nombre: new FormControl(''),
-    clave: new FormControl(''),
-    correo: new FormControl(''),
+  registro: Usuario = {
+    nombre: '',
+    correo: '',
+    clave: '',
+  };
+  usuarioNuevoForm: FormGroup = new FormGroup({
+    nombre: new FormControl(this.registro.nombre, Validators.minLength(3)),
+    correo: new FormControl(this.registro.correo, Validators.minLength(3)),
+    clave: new FormControl(this.registro.clave, Validators.required),
   });
+
   botonActivo: boolean = false;
 
   constructor(private router: Router) {}
 
   onSubmit(): void {
-    const nombreUsaurio = this.usuarioNuevo.get('nombre')?.value;
+    const nombreUsaurio = this.usuarioNuevoForm.get('nombre')?.value;
     this.router.navigate(['mensaje', nombreUsaurio]);
   }
 }
