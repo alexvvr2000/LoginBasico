ed5e53a0d4473f18a726d6b0382a1d47d2ec2c88
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-10-03 23:08:20 -0600

    Corregida ruta principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { MensajeComponent } from './components/mensaje/mensaje.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'login',
     component: LoginFormComponent,
+  },
+  {
+    path: 'mensaje/:nombre',
+    component: MensajeComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
   },
 ];
