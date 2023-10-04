c5ce26d204d8f4ea899c2def7159a2d3ea05f22b
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-10-03 23:45:48 -0600

    Creados campos de nombre, correo y contraseña

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-form',
+  imports: [IonicModule],
+  standalone: true,
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
-export class LoginFormComponent  implements OnInit {
-
-  constructor() { }
+export class LoginFormComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
-
 }
